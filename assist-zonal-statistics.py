e10b15b09b929235266ed68af1c3b84940202408
Simon Wright <smw@ceh.ac.uk>
2016-11-17 13:36:32 +0000

    Initial commit

---
+++ assist-zonal-statistics.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+__author__ = "smw"
+__email__ = "smw@ceh.ac.uk"
+
+
+import os
+import sys
+
+
+import arcpy
+
+
+def main():
+    print('\n\nrunning main()...')
+    #
+
+    fgdb = r'E:\assist\demo-data\arcgis\assist.gdb'
+
+
+
+
+    #
+    print('\n\ndone main().')
+
+
+
+if __name__ == '__main__':
+    main()
