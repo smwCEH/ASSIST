d19c97c7bf15f19abb0a2326828eb71fe90d1c4f
Simon Wright <smw@ceh.ac.uk>
2016-11-17 13:42:53 +0000

    Second commit and push

--- assist-zonal-statistics.py
+++ assist-zonal-statistics.py
@@ -13,8 +13,25 @@ import arcpy
 def main():
     print('\n\nrunning main()...')
     #
-
+    # Define and create file geodatabase if it doesn't exist
     fgdb = r'E:\assist\demo-data\arcgis\assist.gdb'
+    if not arcpy.Exists(fgdb):
+        arcpy.CreateFileGDB_management(out_folder_path=os.path.dirname(fgdb),
+                                       out_name=os.path.basename(fgdb),
+                                       out_version='CURRENT')
+    # Define crop scenario image
+    crop_scenario = r'E:\assist\demo-data\Demo Data for Tool\CroppingScenarios\cropclass_Baseline99.tif'
+    if not arcpy.Exists(crop_scenario):
+        sys.exit('crop_scenario {0} does not exist!'.format(crop_scenario))
+    #
+    # Define region image
+    region = r'E:\assist\demo-data\Demo Data for Tool\Regions\country99.tif'
+    if not arcpy.Exists(region):
+        sys.exit('region {0} does not exist!'.format(region))
+    #
+    # Define arcpy environment settings
+    arcpy.env.workspace = fgdb
+
 
 
 
