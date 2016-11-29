245df2ba011168b02d82b783b1af12dbf817a355
Simon Wright <smw@ceh.ac.uk>
2016-11-29 12:02:57 +0000

    All of example code from Python for Data Analysis added.

--- json-example.py
+++ json-example.py
@@ -3,16 +3,26 @@ __author__ = 'smw'
 __email__ = 'smw@ceh.ac.uk'
 __status__ = 'Development'
 
+
+# Example JSON file mangling from Python for Data Analysis book
+# See: http://shop.oreilly.com/product/0636920023784.do
+# Code scrapped from Chapter 7 pages 212-217 using example USDA Food Database (downloaded from book web site)
+#
+
 import os
 import sys
 
 
 import json
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.width', 120)
+
+
+plt.interactive(False)
 
 
 def main():
@@ -43,6 +53,30 @@ def main():
         nutrients.append(fnuts)
     nutrients = pd.concat(nutrients, ignore_index=True)
     print(nutrients)
+    print(nutrients.info())
+    print(nutrients.duplicated().sum())
+    nutrients = nutrients.drop_duplicates()
+    #
+    col_mapping = {'description' : 'food',
+                   'group'       : 'fgroup'}
+    info=info.rename(columns=col_mapping, copy=False)
+    print(info.info())
+    col_mapping = {'description' : 'nutrient',
+                   'group'       : 'nutgroup'}
+    nutrients = nutrients.rename(columns=col_mapping, copy=False)
+    print(nutrients.info())
+    ndata = pd.merge(nutrients, info, on='id', how='outer')
+    print(ndata.info())
+    print(ndata.ix[30000])
+    #
+    result = ndata.groupby(['nutrient', 'fgroup'])['value'].quantile(0.5)
+    # print(result['Zinc, Zn'])
+    # print(result['Zinc, Zn']).order() # order() method deprecated, replaced with sort_values()
+    print(result['Zinc, Zn']).sort_values()
+    # result['Zinc, Zn'].order().plot(kind='barh')
+    result['Zinc, Zn'].sort_values().plot(kind='barh')
+    plt.subplots_adjust(left=0.45, bottom=0.10, right=0.95, top=0.95)
+    plt.show()
 
 
 
